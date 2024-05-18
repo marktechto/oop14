b222a3dcd8456faaa6541b4d264a4fdff27799d3
MarkoTeshomeTECH <zeanbesa7066@gmail.com>
2024-05-18 23:17:48 +0300

    adding polymorphism using constructor function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
 "use strict";
+
 //||||||||||||||||||||COding Challenge 1/||||||||
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   get speedUS() {
+//     return console.log(` the current speed is  ${this._speed / 1.6} mi/hr`);
+//   }
+//   set speedUs(speed) {
+//     this._speed = speed * 1.6;
+//   }
+// }
+// const ford = new Car("Ford", 120);
+// console.log(ford.speed);
+
+// |||||||||||||||||||| ||||||||||||||||
+
+// ||||||||||||||||||||COding Challenge 1/||||||||
 // const Car = function (make, speed) {
 //   this.make = make;
 //   this.speed = speed;
@@ -12,12 +31,14 @@
 //   this.speed -= 5;
 //   console.log(`the new speed of ${this.make} car is ${this.speed}km/h`);
 // };
-// const BMW = new Car("BMW", 120);
-// const Mereceds = new Car("Mereceds", 95);
-// BMW.accelerate();
-// BMW.brake();
-// Mereceds.accelerate();
-// Mereceds.brake();
+// const ev = new Car("EV", 120);
+// ev._proto__.chargeTo = function () {
+//   battery;
+// };
+
+// EV.accelerate();
+// EV.brake();
+
 //|||||||||||||||||||| ||||||||||||||||
 
 // const Person = function (firstName, birthYear) {
@@ -41,32 +62,67 @@
 // console.log(arr.lastIndexOf(arr));
 // console.log(arr.__proto__);
 // E|| M A S C R I P T C L A S S E S I N J S |||||
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
-  dispAge() {
-    console.log(`${this.fullName} is ${2024 - this.birthYear} years old `);
-  }
-  get age() {
-    return 2024 - this.birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+//   get age() {
+//     return 2024 - this.birthYear;
+//   }
 
-  set fullName(name) {
-    if (name.includes(" ")) {
-      this._fullName = name;
-    } else {
-      alert("you entered full name correctly");
-    }
-  }
-  get fullName() {
-    return console.log(`your name is${this._fullName} `);
-  }
-}
-const markos = new PersonCl("Markos Teshome", 2001);
-console.log(markos.__proto__);
-console.log(markos.__proto__ === PersonCl.prototype);
-console.log(markos.dispAge());
-console.log(markos.age);
-console.log(markos);
+//   set fullName(name) {
+//     if (name.includes(" ")) {
+//       this._fullName = name;
+//     } else {
+//       alert("you entered full name correctly");
+//     }
+//   }
+//   get fullName() {
+//     return console.log(`your name is ${this._fullName} `);
+//   }
+// // }
+// const markos = new PersonCl("Markos Teshome", 2001);
+// console.log(markos.__proto__);
+// console.log(markos.__proto__ === PersonCl.prototype);
+
+// console.log(markos.fullName);
+// console.log(markos);
+// const abrham = new PersonCl("Abrham Shiferaw", 2000);
+// console.log(abrham.fullName);
+
+/// coding CHallenge 3||||||||||||
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`the new speed of ${this.make} car is ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`the new speed of ${this.make} car is ${this.speed}km/h`);
+};
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `the new speed of ${this.make} car is ${this.speed} km/h and the charge is ${this.charge}%`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+console.log(tesla.accelerate());
+tesla.chargeBattery(90);
+console.log(tesla.accelerate());
+console.log(tesla);
