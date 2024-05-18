8a0cd6ea8b89d44e4bfb27bf0ca594d22be2b1ed
MarkoTeshomeTECH <zeanbesa7066@gmail.com>
2024-05-18 16:22:36 +0300

    Added ES6 classes to js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
-"use str ict";
+"use strict";
 //||||||||||||||||||||COding Challenge 1/||||||||
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-Car.prototype.accelerate = function () {
-  const newSpeed = this.speed + 10;
-  console.log(`the new speed of ${this.make} car is ${newSpeed} km/h`);
-};
-Car.prototype.brake = function () {
-  const newSpeed = this.speed - 10;
-  console.log(`the new speed of ${this.make} car is ${newSpeed}km/h`);
-};
-const BMW = new Car("BMW", 120);
-const Mereceds = new Car("Mereceds", 95);
-BMW.accelerate();
-BMW.brake();
-Mereceds.accelerate();
-Mereceds.brake();
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`the new speed of ${this.make} car is ${this.speed} km/h`);
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`the new speed of ${this.make} car is ${this.speed}km/h`);
+// };
+// const BMW = new Car("BMW", 120);
+// const Mereceds = new Car("Mereceds", 95);
+// BMW.accelerate();
+// BMW.brake();
+// Mereceds.accelerate();
+// Mereceds.brake();
+//|||||||||||||||||||| ||||||||||||||||
 
 // const Person = function (firstName, birthYear) {
 //   this.birthYear = birthYear;
@@ -39,3 +40,33 @@ Mereceds.brake();
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // console.log(arr.lastIndexOf(arr));
 // console.log(arr.__proto__);
+// E|| M A S C R I P T C L A S S E S I N J S |||||
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  dispAge() {
+    console.log(`${this.fullName} is ${2024 - this.birthYear} years old `);
+  }
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) {
+      this._fullName = name;
+    } else {
+      alert("you entered full name correctly");
+    }
+  }
+  get fullName() {
+    return console.log(`your name is${this._fullName} `);
+  }
+}
+const markos = new PersonCl("Markos Teshome", 2001);
+console.log(markos.__proto__);
+console.log(markos.__proto__ === PersonCl.prototype);
+console.log(markos.dispAge());
+console.log(markos.age);
+console.log(markos);
